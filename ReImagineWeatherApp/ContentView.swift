2a6220594a7ed58f60c5c0a7f473885106ca2b63
Vans <stevanstc2018@gmail.com>
2024-07-10 17:46:51 +0700

    Initial Commit

---
+++ ReImagineWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReImagineWeatherApp
+//
+//  Created by Stevans Calvin Candra on 10/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReImagineWeatherApp/ReImagineWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReImagineWeatherAppApp.swift
+//  ReImagineWeatherApp
+//
+//  Created by Stevans Calvin Candra on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ReImagineWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
