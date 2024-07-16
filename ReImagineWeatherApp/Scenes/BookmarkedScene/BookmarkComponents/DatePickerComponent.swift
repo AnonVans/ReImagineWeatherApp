23fa3d738306d72a57959da267f4c99b86b52258
Jennifer <57284701+JennLuv@users.noreply.github.com>
2024-07-16 15:40:30 +0700

    date picker component

--- ReImagineWeatherApp/Scenes/BookmarkedScene/BookmarkComponents/DatePickerComponent.swift
+++ ReImagineWeatherApp/Scenes/BookmarkedScene/BookmarkComponents/DatePickerComponent.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct DatePickerComponent: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Text("F")
+                .font(.caption2)
+                .shadow(radius: 10)
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 22, height: 26)
+                    .foregroundColor(.grayTertiary)
+                .shadow(radius: 10)
+                
+                Text("12")
+                    .font(.footnote)
+            }
+            
+            Circle()
+                .foregroundColor(Color.paleGreenCircle)
+                .frame(width: 8)
+                .shadow(radius: 10)
+            
+        }
+        .frame(width: 22, height: 80)
     }
 }
 
