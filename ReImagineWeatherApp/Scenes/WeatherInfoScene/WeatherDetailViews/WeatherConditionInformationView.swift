50a8b1be8b8a676d61754d59775d7d24795d9007
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-07-16 15:25:37 +0700

    buat information detail card, weather condition information view, location service manager

---
+++ ReImagineWeatherApp/Scenes/WeatherInfoScene/WeatherDetailViews/WeatherConditionInformationView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherConditionInformationView.swift
+//  ReImagineWeatherApp
+//
+//  Created by Haliza Syafa Oktaviani on 16/07/24.
+//
+
+import SwiftUI
+
+struct WeatherConditionInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherConditionInformationView()
+}
