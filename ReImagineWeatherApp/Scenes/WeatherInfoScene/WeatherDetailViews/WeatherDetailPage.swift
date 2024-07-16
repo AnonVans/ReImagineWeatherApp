c9e12ff3f156449307254f6c22ea77af311070c4
Jennifer <57284701+JennLuv@users.noreply.github.com>
2024-07-16 16:08:43 +0700

    weather detail page template

--- ReImagineWeatherApp/Scenes/WeatherInfoScene/WeatherDetailViews/WeatherDetailPage.swift
+++ ReImagineWeatherApp/Scenes/WeatherInfoScene/WeatherDetailViews/WeatherDetailPage.swift
@@ -9,7 +9,79 @@ import SwiftUI
 
 struct WeatherDetailPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack (spacing: 15){
+                Text("Green Office Park")
+                    .font(.title3)
+                    .bold()
+                Text("Cisauk, Tangerang")
+                    .font(.footnote)
+                    .foregroundStyle(.arsenic)
+                    .padding(.top, -10)
+                
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 15) {
+                        ForEach(0..<20) { _ in
+                            Rectangle()
+                                .frame(width: 22)
+                                .foregroundColor(.grayTertiary)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+                .frame(width: 361, height: 59)
+                    
+                Divider()
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 15) {
+                        ForEach(0..<20) { _ in
+                            Rectangle()
+                                .frame(width: 52)
+                                .foregroundColor(.grayTertiary)
+                                .cornerRadius(15)
+                        }
+                    }
+                }
+                .frame(width: 361, height: 59)
+                
+                Divider()
+                    .padding(.bottom)
+                
+                
+                Image(systemName: "cloud.sun.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 240)
+                    .symbolRenderingMode(.multicolor)
+                    .padding(.bottom)
+                
+                Text("Partly Cloudy")
+                    .font(.headline)
+                Text("29°")
+                    .font(.subheadline)
+                    .padding(.bottom)
+                
+                VStack (spacing: 20){
+                    Rectangle()
+                        .frame(width: 361, height: 95)
+                        .cornerRadius(20)
+                    
+                    ForEach(0..<3) { _ in
+                        Rectangle()
+                            .frame(width: 361, height: 109)
+                            .cornerRadius(20)
+                    }
+                }
+                .foregroundColor(.arsenic)
+                
+            }
+            .padding()
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.cream, .skyBlue]), startPoint: .top, endPoint: .bottom)
+        )
     }
 }
 
