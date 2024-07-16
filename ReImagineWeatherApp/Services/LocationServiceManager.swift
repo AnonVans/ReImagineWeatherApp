fe034c320e9270edd70ea776ecd526e5d55e34c3
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-07-16 15:32:54 +0700

    bikin information detail card, weather condition information view, location service manager

--- ReImagineWeatherApp/Services/LocationServiceManager.swift
+++ ReImagineWeatherApp/Services/LocationServiceManager.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+import CoreLocation
+
+protocol CoreLocationServiceDelegate {
+    func getCoordinate(coordinate: String)
+}
+
+//location model
+
+struct LocationModel {
+    let latitude: Double
+    let longitude: Double
+    var address: String?
+}
+
+class LocationServiceManager: NSObject, CLLocationManagerDelegate {
+    var currentLocation: LocationModel?
+//    var coordinatesFromAddress: CLLocationCoordinate2D?
+    var coordinatesFromAddress: (latitude: Double, longitude: Double)?
+    var errorMessage: String?
+
+    private var locationManager: CLLocationManager
+
+    override init() {
+        locationManager = CLLocationManager()
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+
+    func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+
+    func fetchAddress(from location: CLLocation, completion: @escaping (String?) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("Error reverse geocoding location: \(error.localizedDescription)")
+                completion(nil)
+            } else {
+                let placemark = placemarks?.first
+                let address = [placemark?.name, placemark?.locality, placemark?.administrativeArea, placemark?.country].compactMap { $0 }.joined(separator: ", ")
+                completion(address)
+            }
+        }
+    }
+
+    func fetchCoordinates(from address: String, completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { placemarks, error in
+            if let error = error {
+                print("Error geocoding address: \(error.localizedDescription)")
+                completion(nil)
+            } else {
+                let location = placemarks?.first?.location
+                print("Geocoded address: \(String(describing: location?.coordinate))")
+                completion(location?.coordinate)
+            }
+        }
+    }
+
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        fetchAddress(from: location) { [weak self] address in
+            DispatchQueue.main.async {
+                self?.currentLocation = LocationModel(
+                    latitude: location.coordinate.latitude,
+                    longitude: location.coordinate.longitude,
+                    address: address
+                )
+            }
+        }
+        print("Updated location: Latitude \(location.coordinate.latitude), Longitude \(location.coordinate.longitude)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        DispatchQueue.main.async {
+            self.errorMessage = "Failed to find user's location: \(error.localizedDescription)"
+        }
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+}
