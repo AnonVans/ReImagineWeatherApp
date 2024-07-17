26b39c230dfa79b4b944ed6ac972818d1aa11633
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-07-17 13:35:52 +0700

    bikin safety card component

--- ReImagineWeatherApp/Scenes/WeatherInfoScene/WeatherDetailComponents/SafetyCardComponent.swift
+++ ReImagineWeatherApp/Scenes/WeatherInfoScene/WeatherDetailComponents/SafetyCardComponent.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct SafetyCardComponent: View {
+    
+//    var dataType: String = ""
+//    var status: Status
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        ZStack {
+//            HStack(alignment: .top) {
+//                
+//                VStack(alignment: .leading, spacing: 20) {
+//                    HStack (alignment: .top) {
+//                        Text(status.status)
+//                            .font(.subheadline)
+//                            .fontWeight(.bold)
+//                        Spacer()
+//                        Circle()
+//                            .frame(width: 20)
+//                            .foregroundColor(status.circleColor)
+//                            .shadow(radius: 2)
+//                        
+//                    }
+//                    Text(status.information)
+//                        .foregroundColor(.arsenic)
+//                        .font(.caption)
+//                    
+//                    HStack {
+//                        
+//                        let appWeather = SunnyLight(id: 1, location: "Location", city: "City", time: "12:00", UVI: 5, AQI: 50, status: status)
+//                        
+//                        InformationDetailCard(status: status, dataType: "UVI", appWeather: appWeather)
+//                        Spacer()
+//                        
+//                        InformationDetailCard(status: status, dataType: "AQI", appWeather: appWeather)
+//                        Spacer()
+//                        
+//                        InformationDetailCard(status: status, dataType: "Weather", appWeather: appWeather)
+//                        
+//                    }
+//                }
+//                .padding()
+//            }
+//        }
+//        .frame(width: .infinity)
+//        .background(Color.grayQuaternary)
+//        .cornerRadius(20)
+        Text("ini nampilin status detail component")
     }
 }
 
-#Preview {
-    SafetyCardComponent()
-}
+//#Preview {
+//    SafetyCardComponent()
+//}
